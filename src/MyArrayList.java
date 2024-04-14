430404660b37ff83d9da19ca7fbe7cd9fca00335
Rabat <rabatkarabek8@gmail.com>
2024-04-14 23:06:39 +0500

    improved MyArrayList

--- src/MyArrayList.java
+++ src/MyArrayList.java
@@ -1,9 +1,20 @@
 import java.util.Iterator;
 
-public class MyArrayList<T> implements MyList<T>{
+public class MyArrayList<T> extends Comparable<T> implements MyList<T>{
     private Object[] arr;
     private int length = 0;
     private static final int capacity = 10;
+    public MyArrayList(){
+        this(capacity);
+        length = 0;
+    }
+
+    public MyArrayList(int initialCapacity) {
+        if(initialCapacity<0)
+            throw new IllegalArgumentException("Illegal capacity: " + initialCapacity);
+        arr = new Object[initialCapacity];
+    }
+
     private void IncreaseCapacity(){
         Object[] newarr = new Object[arr.length * 2];
         for(int i =0;i<length;i++) {
@@ -18,67 +29,100 @@ public class MyArrayList<T> implements MyList<T>{
             IncreaseCapacity();
         arr[length++] = item;
     }
-
-    @Override
-    public void set(int index, Object item) {
+    public void checkup(int index){
         if(index< 0 || index>=length){
-            throw new IndexOutOfBoundsException("Index: " + index + )
+            throw new IndexOutOfBoundsException("index: " + index + "size: " + length );
         }
     }
+    @Override
+    public void set(int index, T item) {
+        checkup(index);
+        arr[index] = item;
+    }
+
 
     @Override
-    public void add(int index, Object item) {
-
+    public void add(int index, T item) {
+        checkup(index);
+        if(length == arr.length)
+            IncreaseCapacity();
+        System.arraycopy(arr, index, arr, index + 1, length - index);
+        arr[index] = item;
+        length++;
     }
 
     @Override
-    public void addFirst(Object item) {
-
+    public void addFirst(T item) {
+        add(0,item);
     }
 
     @Override
-    public void addLast(Object item) {
-
+    public void addLast(T item) {
+        add(item);
     }
 
     @Override
     public T get(int index) {
-        return null;
+        checkup(index);
+        return (T) arr[index];
     }
 
     @Override
     public T getFirst() {
-        return null;
+        if(length==0){
+            throw new IndexOutOfBoundsException("List does not exist");
+        }
+        return (T) arr[0];
     }
 
     @Override
     public T getLast() {
-        return null;
+        if(length==0){
+            throw new IndexOutOfBoundsException("List does not exist");
+        }
+        return (T) arr[length-1];
     }
 
     @Override
     public void remove(int index) {
-
+        checkup(index);
+        int numberMoved= length - index - 1;
+        if (numberMoved > 0) {
+            System.arraycopy(arr, index + 1, arr, index, numberMoved);
+        }
+        arr[--length] = null;
     }
 
     @Override
     public void removeFirst() {
-
+        remove(0);
     }
 
     @Override
     public void removeLast() {
-
+        remove(length-1);
     }
 
     @Override
-    public void sort() {
-
+    public void sort() { // sorting by descending
+        for(int i=0; i<length;i++){
+            for(int j= 0; j<length-i-1;j++){
+                if(((Comparable)arr[j]).compareTo(arr[j+1]) > 0){
+                    Object temp = arr[j+1];
+                    arr[j+1] = arr[j];
+                    arr[j] = temp;
+                }
+            }
+        }
     }
 
     @Override
-    public int index0f(Object obiect) {
-        return 0;
+    public int index0f(Object object) {
+        for(int i=0;i<length;i++){
+            if(arr[i].equals(object)){
+                return i;
+            }
+        }
     }
 
     @Override

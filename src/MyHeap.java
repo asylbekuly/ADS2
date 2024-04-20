4b7f65f6332ad1c828db37659a2299fad4610dd4
Rabat <rabatkarabek8@gmail.com>
2024-04-20 21:33:55 +0500

    code improving

--- src/MyHeap.java
+++ src/MyHeap.java
@@ -1,3 +1,28 @@
-public class MyHeap {
+public class MyHeap<T extends Comparable<T>> {
+    MyArrayList<T> heap = new MyArrayList<>();
+    public MyHeap(){
+
+    }
+    public void empty(){
+        heap.clear();
+    }
+    public int size(){
+        return heap.size();
+    }
+    public T getMax(){
+        return heap.get(0);
+    }
+    public T extractMax() { // gets max and swaps with last element then removes last element
+        T max = heap.get(0);
+        swap(0, heap.size() - 1);
+        heap.remove(heap.size() - 1);
+        heapify(0);
+        return max;
+    }
+    public void insert(T item){
+        heap.add(item);
+        int i = heap.size()-1;
+        heapify(i);
+    }
 
 }
